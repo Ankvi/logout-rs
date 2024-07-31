bb66cce960f0d61f05d422be4ae92c8682f1527e
Andreas Kvist <kvistcake@gmail.com>
2024-07-31 09:38:18 +0200

    maybe this works? lol. rust is wierd

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,13 +14,30 @@ fn notify() {
     println!("Notification sent");
 }
 
+struct SystemCommand {
+    exec: String,
+    args: Vec<String>
+}
+
 #[tauri::command]
 fn run_command(command: &str) {
-    let commands = HashMap::from([
-        ("lock", "loginctl lock-session"),
-        ("reboot", "systemctl reboot"),
-        ("shutdown", "systemctl poweroff"),
-        ("log-out", "loginctl terminate-user $USER"),
+    let commands: HashMap<&str, SystemCommand> = HashMap::from([
+        ("lock", SystemCommand {
+            exec: "loginctl".to_string(),
+            args: ["lock-session".to_string()].to_vec()
+        }),
+        ("reboot", SystemCommand {
+            exec: "systemctl".to_string(),
+            args: ["reboot".to_string()].to_vec()
+        }),
+        ("shutdown", SystemCommand {
+            exec: "systemctl".to_string(),
+            args: ["poweroff".to_string()].to_vec()
+        }),
+        ("log-out", SystemCommand {
+            exec: "loginctl".to_string(),
+            args: ["terminate-user".to_string(), "$USER".to_string()].to_vec()
+        }),
     ]);
 
     if !commands.contains_key(command) {
@@ -28,10 +45,16 @@ fn run_command(command: &str) {
         return;
     }
 
-    let system_command = commands.get(command);
-    println!("Running command '{}'", system_command.unwrap());
+    let system_command = commands.get(command).unwrap();
+    println!("Running command '{}'", system_command.exec);
 
-    Command::new(system_command.unwrap())
+    let mut command = Command::new(system_command.exec.clone());
+
+    for arg in system_command.args.iter() {
+        command.arg(arg);
+    }
+
+    command
         .spawn()
         .expect("Could not execute command");
 }
